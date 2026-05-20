8caf3c6c73fb58bc96fc3448a5c5341236f358c1
JYZ-LESLIE <jyz3253027@gmail.com>
2026-05-20 11:59:21 +0800

    Make reimbursement pool the default import source
    Hide old scan-ledger merging from daily scans, preserve cumulative pool rows, and keep mailbox-date scans from dropping newly received invoices by invoice issue date.

--- content_ops/mac/build_invoice_app.sh
+++ content_ops/mac/build_invoice_app.sh
@@ -52,9 +52,9 @@ cat > "$CONTENTS/Info.plist" <<'PLIST'
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.1.6</string>
+  <string>0.1.7</string>
   <key>CFBundleVersion</key>
-  <string>20260520.1</string>
+  <string>20260520.2</string>
   <key>LSMinimumSystemVersion</key>
   <string>26.0</string>
   <key>NSHighResolutionCapable</key>
